7db333544d48eb1f961c44a62d4112ecc4db8fda
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-09 20:09:23 +0300

    Passing a thing that has a behavior NOT just a behavior

--- src/dev/fhhf/Greeter.java
+++ src/dev/fhhf/Greeter.java
@@ -1,12 +1,13 @@
 package dev.fhhf;
 
 public class Greeter {
-
+    // Passing a thing that has a behavior NOT just a behavior
     public void greet(Greeting greeting){
         greeting.perform();
     }
 
     public static void main(String[] args) {
+
         Greeter greeter = new Greeter();
         HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
         greeter.greet(helloWorldGreeting);
