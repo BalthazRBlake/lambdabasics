726b1e6272cdc4efa07995b4f7a2f2014d98abb3
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-10 23:17:10 +0300

    package java.util.function | Out of the box interfaces

--- src/dev/fhhf/lambdaexercise/Unit1ExerciseSolutionJava8.java
+++ src/dev/fhhf/lambdaexercise/Unit1ExerciseSolutionJava8.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Unit1ExerciseSolutionJava8 {
 
@@ -31,9 +32,9 @@ public class Unit1ExerciseSolutionJava8 {
         printConditionally(people, p -> p.getFirstName().startsWith("L"));
     }
 
-    private static void printConditionally(List<Person> people, Condition condition){
+    private static void printConditionally(List<Person> people, Predicate<Person> predicate){
         for(Person p : people){
-            if(condition.test(p)) {
+            if(predicate.test(p)) {
                 System.out.println(p);
             }
         }
