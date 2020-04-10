b45a2f183114e4201db6a74769f30564cc0e127f
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-10 18:32:51 +0300

    Type inference Lambda

---
+++ src/dev/fhhf/TypeInferenceExample.java
@@ -0,0 +1,13 @@
+package dev.fhhf;
+
+public class TypeInferenceExample {
+
+    public static void main(String[] args) {
+        StringLengthLambda myLambda = (String s) -> s.length();
+        System.out.println(myLambda.getLength("Hello Lambda"));
+    }
+
+    interface StringLengthLambda {
+        int getLength(String s);
+    }
+}
