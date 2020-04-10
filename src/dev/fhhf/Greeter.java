e23ad059b16e3634f21fe80c995b3fb3098e2478
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-10 18:07:52 +0300

    Lambda expression behaves as an implementation of the interface

--- src/dev/fhhf/Greeter.java
+++ src/dev/fhhf/Greeter.java
@@ -9,9 +9,12 @@ public class Greeter {
     public static void main(String[] args) {
 
         Greeter greeter = new Greeter();
-        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
-        greeter.greet(helloWorldGreeting);
 
+        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
         Greeting myLambdaFunction = () -> System.out.println("Hello World Lambda");
+
+        //greeter.greet(helloWorldGreeting);
+        helloWorldGreeting.perform();
+        myLambdaFunction.perform();
     }
 }
