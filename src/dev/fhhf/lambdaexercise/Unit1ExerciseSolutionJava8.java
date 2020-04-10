e3409f1c961befa603d92e2fd74c1b1085671e0f
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-10 23:17:19 +0300

    package java.util.function | Out of the box interfaces

--- src/dev/fhhf/lambdaexercise/Unit1ExerciseSolutionJava8.java
+++ src/dev/fhhf/lambdaexercise/Unit1ExerciseSolutionJava8.java
@@ -2,7 +2,6 @@ package dev.fhhf.lambdaexercise;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
