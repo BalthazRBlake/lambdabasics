decdf4872dd36607313648323e3f65186bc763be
FHHF <fabianhumbertohernandez@gmail.com>
2020-05-06 18:59:05 +0300

    this reference in Anonymous Inner Class

--- src/dev/fhhf/unit2/ThisReferenceExample.java
+++ src/dev/fhhf/unit2/ThisReferenceExample.java
@@ -10,11 +10,18 @@ public class ThisReferenceExample {
 
         ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
 
+        //System.out.println(this); // this can't be reference in Static methods
+
         thisReferenceExample.doProcess(10, new Process() {
             @Override
             public void process(int i) {
                 System.out.println("Value of i: " + i);
-                System.out.println(this);
+                System.out.println(this); // this works because it points to new instance of Process.
+            }
+
+            @Override
+            public String toString() {
+                return "This is the anonymous inner class";
             }
         });
     }
