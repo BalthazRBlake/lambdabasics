dda52bbb345743f26902dd15a95fb4a8b72b5193
FHHF <fabianhumbertohernandez@gmail.com>
2020-05-06 19:14:16 +0300

    this reference inside lambda points to the same instance caller

--- src/dev/fhhf/unit2/ThisReferenceExample.java
+++ src/dev/fhhf/unit2/ThisReferenceExample.java
@@ -6,14 +6,27 @@ public class ThisReferenceExample {
         p.process(i);
     }
 
+    public void execute() {
+        doProcess(10, i -> {
+            System.out.println("Value of i: " + i);
+            System.out.println(this); // points to the instance of the method calling the function lambda
+        });
+    }
+
     public static void main(String[] args) {
 
         ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
 
-        thisReferenceExample.doProcess(10, i -> {
+        /*thisReferenceExample.doProcess(10, i -> {
             System.out.println("Value of i: " + i);
-            System.out.println(this); // this does'n work because it points to Static main.
+            //System.out.println(this); // this does'n work because it points to Static main.
             //Does not override the main this reference, nor creates an instance of a Non-StaticClass
-        });
+        });*/
+
+        thisReferenceExample.execute();
+    }
+
+    public String toString(){
+        return "This is the main ThisReferenceExample Class instance";
     }
 }
